f30a19880f0090fc680bf3ef87d6fb0347bbaa25
James Florez <jandrey2002@gmail.com>
2024-03-07 14:40:14 -0500

    Creando proyecto

---
+++ src/main/java/com/autobuses/reservacion/ReservacionDeViajesApiApplication.java
@@ -0,0 +1,13 @@
+package com.autobuses.reservacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservacionDeViajesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservacionDeViajesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autobuses/reservacion/ReservacionDeViajesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autobuses.reservacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservacionDeViajesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
